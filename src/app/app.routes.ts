647176970a868973fec0380147446293643b6283
AlcedoEllyn <alcedoellyn22@gmail.com>
2026-03-18 20:49:10 +0700

    configured routing with fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,11 @@ import { SuppliersListComponent } from './comps/suppliers-list.component/supplie
 import { SupplierDetailsComponent } from './comps/supplier-details.component/supplier-details.component';
 
 export const routes: Routes = [
-  // 1. Default path: Redirects to /suppliers when the app first opens
   { path: '', redirectTo: 'suppliers', pathMatch: 'full' },
 
-  // 2. The main list path
   { path: 'suppliers', component: SuppliersListComponent },
 
-  // 3. The details path with a "parameter" (:id)
   { path: 'suppliers/:id', component: SupplierDetailsComponent },
 
-  // 4. Wildcard path (Optional): Redirects any unknown URL back to the list
   { path: '**', redirectTo: 'suppliers' }
 ];
